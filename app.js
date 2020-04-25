5d31cd50a436347c150e67d8d52211eba0e96ba7
Dou Jones <misterchiefexecutiveofficer@yahoo.com>
2020-04-25 12:20:27 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,48 @@
+const navbar = document.querySelector("#nav");
+const navBtn = document.querySelector("#nav-btn");
+const closeBtn = document.querySelector("#close-btn");
+const sidebar = document.querySelector("#sidebar");
+const date = document.querySelector("#date");
+let c = document.querySelector(".clock");
+let d = document.querySelector(".dates");
+
+
+// add fixed class to navbar
+window.addEventListener("scroll", function () {
+  if (window.pageYOffset > 83) {
+    navbar.classList.add("top");
+  } else {
+    navbar.classList.remove("top");
+  }
+});
+// show sidebar
+navBtn.addEventListener("click", function () {
+  sidebar.classList.add("show-sidebar");
+});
+closeBtn.addEventListener("click", function () {
+  sidebar.classList.remove("show-sidebar");
+});
+
+
+
+// Times and Dates
+setInterval(() => {
+  let today = new Date();
+  let hh = today.getHours();
+  let mm = today.getMinutes();
+  let ss = today.getSeconds();
+  let dates = today.toDateString();
+
+  hh = (hh == 0) ? 12 : hh;
+  hh = (hh > 12) ? hh - 12 : hh;
+
+  c.innerText = `${helloWill(hh)} : ${helloWill(mm)} : ${helloWill(ss)}`;
+  d.innerText = dates;
+}, 1000);
+
+function helloWill(n){
+  return n.toString().padStart(2, "0");
+}
+
+// set year
+date.innerHTML = new Date().getFullYear();
